8befa5e6685796468d5fce972e2de543c536be69
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-02-10 18:19:01 +0100

    create new page

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,5 +1,11 @@
+import React from "react"
 import "./assets/styles/index.css"
+import Home from "./pages/Home.tsx"
 
 export default function App() {
-  return <h1>Hello world!</h1>
+  return (
+    <React.Fragment>
+      <Home />
+    </React.Fragment>
+  )
 }
