067c3fc50fe6de4e0c2122ee336462b886113606
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-02-12 22:31:34 +0100

    adding img for the header

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react"
 import "./assets/styles/index.css"
 import Home from "./pages/Home.tsx"
+import Header from "./layouts/Header.tsx"
 
 export default function App() {
   return (
     <React.Fragment>
+      <Header />
       <Home />
     </React.Fragment>
   )
---
+++ front/src/layouts/Header.tsx
@@ -0,0 +1,11 @@
+import SFiveIMG from "../assets/images/sfive_icone.png"
+
+export default function Header() {
+  return (
+    <div>
+      <span>
+        <img src={SFiveIMG} alt="SFive" />
+      </span>
+    </div>
+  )
+}
