58192f76a45541e642a3a870b32310ef2eede424
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-02-18 17:52:52 +0100

    remove Link to booking

--- front/src/pages/Booking.tsx
+++ front/src/pages/Booking.tsx
@@ -1,5 +1,3 @@
-import { Link } from "react-router-dom"
-
 const people = [
   {
     ref: "#580911",
@@ -60,53 +58,51 @@ const people = [
 export default function Booking() {
   return (
     <ul role="list" className="divide-y divide-gray-100">
-      {people.map((person, index) => (
-        <Link key={index} to={`/booking/${index}`}>
-          <li
-            key={person.tel}
-            className="flex flex-col sm:flex-row justify-between gap-x-6 px-4 py-5 border-solid border-b-2 hover:bg-gray-100"
-          >
-            <div className="flex flex-col sm:flex-row gap-x-4 items-start sm:items-center w-full">
-              <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-6 text-gray-800">
-                  {person.ref} - {person.name}
+      {people.map((person) => (
+        <li
+          key={person.tel}
+          className="flex flex-col sm:flex-row justify-between gap-x-6 px-4 py-5 border-solid border-b-2 hover:bg-gray-100"
+        >
+          <div className="flex flex-col sm:flex-row gap-x-4 items-start sm:items-center w-full">
+            <div className="min-w-0 flex-auto">
+              <p className="text-sm font-semibold leading-6 text-gray-800">
+                {person.ref} - {person.name}
+              </p>
+              <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                <span className="font-bold">Tél : </span>
+                {person.tel}
+              </p>
+              <br />
+              {person.date && (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  <span className="font-bold">Date : </span>
+                  {person.date}
                 </p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                  <span className="font-bold">Tél : </span>
-                  {person.tel}
+              )}
+              {person.time && (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  <span className="font-bold">Créneau : </span>
+                  {person.time}
                 </p>
-                <br />
-                {person.date && (
-                  <p className="mt-1 text-xs leading-5 text-gray-500">
-                    <span className="font-bold">Date : </span>
-                    {person.date}
-                  </p>
-                )}
-                {person.time && (
-                  <p className="mt-1 text-xs leading-5 text-gray-500">
-                    <span className="font-bold">Créneau : </span>
-                    {person.time}
-                  </p>
-                )}
-                {person.field && (
-                  <p className="mt-1 text-xs leading-5 text-gray-500">
-                    <span className="font-bold">Terrain : </span>
-                    {person.field}
-                  </p>
-                )}
-                {person.price && (
-                  <p className="mt-1 text-xs leading-5 text-gray-500">
-                    <span className="font-bold">Prix : </span>
-                    {person.price}
-                  </p>
-                )}
-              </div>
+              )}
+              {person.field && (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  <span className="font-bold">Terrain : </span>
+                  {person.field}
+                </p>
+              )}
+              {person.price && (
+                <p className="mt-1 text-xs leading-5 text-gray-500">
+                  <span className="font-bold">Prix : </span>
+                  {person.price}
+                </p>
+              )}
             </div>
-            <button className="bg-gray-800 w-24 h-10 ml-auto hover:bg-gray-900 text-white font-semibold py-2 px-4 mt-auto rounded-md sm:w-auto">
-              Annuler
-            </button>
-          </li>
-        </Link>
+          </div>
+          <button className="bg-gray-800 w-24 h-10 ml-auto hover:bg-gray-900 text-white font-semibold py-2 px-4 mt-auto rounded-md sm:w-auto">
+            Annuler
+          </button>
+        </li>
       ))}
     </ul>
   )
