f517b43ec8ad34c5a31d30a4c4661ec25a00499e
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-02-21 17:14:24 +0100

    delete .

--- front/src/pages/Profile.tsx
+++ front/src/pages/Profile.tsx
@@ -24,7 +24,7 @@ export default function Profile() {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Mon profil.
+            Mon profil
           </h2>
           <p className="mt-2 text-lg leading-8 text-gray-600">
             Informations détaillées.
