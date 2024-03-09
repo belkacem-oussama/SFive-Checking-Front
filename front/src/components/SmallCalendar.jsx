b6ffd0edee85f67435c184f3ef10a87d68b2fc0f
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-03-09 23:12:39 +0100

    add default value && update buttonText

--- front/src/components/SmallCalendar.jsx
+++ front/src/components/SmallCalendar.jsx
@@ -2,10 +2,13 @@ import { Datepicker } from "flowbite-react"
 import { useState } from "react"
 
 export default function SmallCalendar() {
-  const [selectedDate, setSelectedDate] = useState("")
+  // Obtention de la date du jour avec le même format que formattedDate
+  const options = { weekday: "long", day: "2-digit", month: "long" }
+  const currentDate = new Date().toLocaleDateString("fr-FR", options)
+
+  const [selectedDate, setSelectedDate] = useState(currentDate)
 
   const handleDatePickerChange = (date) => {
-    const options = { weekday: "long", day: "2-digit", month: "long" }
     const formattedDate = date.toLocaleDateString("fr-FR", options)
     setSelectedDate(formattedDate)
     console.log(formattedDate)
@@ -14,9 +17,11 @@ export default function SmallCalendar() {
   return (
     <div className="p-2">
       <Datepicker
+        defaultDate={new Date()}
         weekStart={1}
         labelTodayButton="Aujourd'hui"
-        showClearButton={false}
+        showClearButton={true}
+        labelClearButton="Fermer"
         language="fr-FR"
         onSelectedDateChanged={handleDatePickerChange}
         value={selectedDate}
--- front/src/pages/Home.jsx
+++ front/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import FieldCalendar from "../components/FieldCalendar.jsx"
+import SmallCalendar from "../components/SmallCalendar.jsx"
 
 export default function Home() {
   return (
     <div>
       <div className="text-center py-4 border-b mx-2 border-gray-900/10 pb-3 md:text-left">
         <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl md:ml-2"></h2>
+        <SmallCalendar />
       </div>
       <div>
         <FieldCalendar />
