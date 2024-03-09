558d6c18d1752def3ee1cb7c45ba1bc4d456684b
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-03-09 23:05:51 +0100

    add new formCalendar

--- front/src/components/SmallCalendar.jsx
+++ front/src/components/SmallCalendar.jsx
@@ -1,26 +1,26 @@
+import { Datepicker } from "flowbite-react"
 import { useState } from "react"
-import "dayjs/locale/fr"
-import { DemoContainer } from "@mui/x-date-pickers/internals/demo"
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
-import { DatePicker } from "@mui/x-date-pickers/DatePicker"
 
 export default function SmallCalendar() {
-  const [dateValue, setDateValue] = useState(null)
+  const [selectedDate, setSelectedDate] = useState("")
+
+  const handleDatePickerChange = (date) => {
+    const options = { weekday: "long", day: "2-digit", month: "long" }
+    const formattedDate = date.toLocaleDateString("fr-FR", options)
+    setSelectedDate(formattedDate)
+    console.log(formattedDate)
+  }
 
   return (
-    <div className="p-2 w-full">
-      <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="fr">
-        <DemoContainer components={["DatePicker"]}>
-          <DatePicker
-            sx={{
-              width: 1,
-            }}
-            value={dateValue}
-            onChange={(newValue) => setDateValue(newValue)}
-          />
-        </DemoContainer>
-      </LocalizationProvider>
+    <div className="p-2">
+      <Datepicker
+        weekStart={1}
+        labelTodayButton="Aujourd'hui"
+        showClearButton={false}
+        language="fr-FR"
+        onSelectedDateChanged={handleDatePickerChange}
+        value={selectedDate}
+      />
     </div>
   )
 }
