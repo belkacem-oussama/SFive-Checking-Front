e34273951089057ff7c95401e0fb9c4556bdf8cd
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-03-11 07:37:02 +0100

    console errors fix

--- front/src/layouts/Header.jsx
+++ front/src/layouts/Header.jsx
@@ -155,21 +155,23 @@ export default function Header() {
           <Disclosure.Panel className="sm:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
-                <Link to={item.href} key={item.id}>
-                  <Disclosure.Button
-                    key={item.name}
-                    as="a"
-                    className={classNames(
-                      item.current
-                        ? "bg-gray-900 text-white"
-                        : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                      "block rounded-md px-3 py-2 text-base font-medium"
-                    )}
-                    aria-current={item.current ? "page" : undefined}
-                  >
-                    {item.name}
-                  </Disclosure.Button>
-                </Link>
+                <div key={item.id}>
+                  <Link to={item.href} key={item.id}>
+                    <Disclosure.Button
+                      key={item.name}
+                      as="div"
+                      className={classNames(
+                        item.current
+                          ? "bg-gray-900 text-white"
+                          : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                        "block rounded-md px-3 py-2 text-base font-medium"
+                      )}
+                      aria-current={item.current ? "page" : undefined}
+                    >
+                      {item.name}
+                    </Disclosure.Button>
+                  </Link>
+                </div>
               ))}
             </div>
           </Disclosure.Panel>
