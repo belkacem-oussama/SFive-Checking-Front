ed4f2732e9b16b2bc64065c0a9b8db3272a69f6c
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-03-14 15:15:49 +0100

    cancel booking

---
+++ front/src/components/Popup.jsx
@@ -0,0 +1,34 @@
+import React from "react"
+
+export default function Popup({
+  handleCancel,
+  bookingId,
+  handleConfirmCancellation,
+}) {
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
+      <div className="bg-white p-8 rounded-lg shadow-md">
+        <h2 className="text-lg font-semibold mb-4">
+          Annuler la réservation #{bookingId}
+        </h2>
+        <p className="text-gray-700 mb-6">
+          Êtes-vous sûr de vouloir annuler cette réservation ?
+        </p>
+        <div className="flex justify-end">
+          <button
+            onClick={handleCancel}
+            className="font-bold px-4 py-2 bg-gray-200 text-gray-800 rounded-md mr-4 hover:bg-gray-300"
+          >
+            Annuler
+          </button>
+          <button
+            onClick={handleConfirmCancellation}
+            className="font-bold px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600"
+          >
+            Confirmer
+          </button>
+        </div>
+      </div>
+    </div>
+  )
+}
--- front/src/pages/Booking.jsx
+++ front/src/pages/Booking.jsx
@@ -1,53 +1,84 @@
 import Button from "../components/Button.jsx"
 import booking from "../assets/json/booking.json"
+import { useState } from "react"
+import Popup from "../components/Popup.jsx"
 
 export default function Booking() {
+  const [showPopUp, setShowPopUp] = useState(false)
+  const [bookingId, setBookingId] = useState(null)
+
+  const handleDropBooking = (id) => {
+    setShowPopUp(true)
+    setBookingId(id)
+  }
+
+  const handleConfirmCancellation = () => {
+    console.log("Réservation annulée :", bookingId)
+    setShowPopUp(false)
+  }
+
+  const handleCancel = () => {
+    setShowPopUp(false)
+  }
+
   return (
-    <ul role="list" className="divide-y divide-gray-100">
-      {booking.map((book) => (
-        <li
-          key={book.phone}
-          className="flex flex-col sm:flex-row justify-between gap-x-6 px-4 py-5 border-solid border-b-2 hover:bg-gray-100"
-        >
-          <div className="flex flex-col sm:flex-row gap-x-4 items-start sm:items-center w-full">
-            <div className="min-w-0 flex-auto">
-              <p className="text-sm font-semibold leading-6 text-gray-800">
-                #{book.id} - {book.firstname} {book.lastname}
-              </p>
-              <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                <span className="font-bold">Tél : </span>
-                {book.phone}
-              </p>
-              <br />
-              {book.date && (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  <span className="font-bold">Date : </span>
-                  {book.date}
+    <>
+      {showPopUp && (
+        <Popup
+          bookingId={bookingId}
+          handleConfirmCancellation={handleConfirmCancellation}
+          handleCancel={handleCancel}
+        />
+      )}
+      <ul role="list" className="divide-y divide-gray-100">
+        {booking.map((book) => (
+          <li
+            key={book.phone}
+            className="flex flex-col sm:flex-row justify-between gap-x-6 px-4 py-5 border-solid border-b-2 hover:bg-gray-100"
+          >
+            <div className="flex flex-col sm:flex-row gap-x-4 items-start sm:items-center w-full">
+              <div className="min-w-0 flex-auto">
+                <p className="text-sm font-semibold leading-6 text-gray-800">
+                  #{book.id} - {book.firstname} {book.lastname}
                 </p>
-              )}
-              {book.startedTime && book.endedTime && (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  <span className="font-bold">Créneau : </span>
-                  {book.startedTime}h - {book.endedTime}h
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                  <span className="font-bold">Tél : </span>
+                  {book.phone}
                 </p>
-              )}
-              {book.field && (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  <span className="font-bold">Terrain : </span>
-                  {book.field}
-                </p>
-              )}
-              {book.price && (
-                <p className="mt-1 text-xs leading-5 text-gray-500">
-                  <span className="font-bold">Prix : </span>
-                  {book.price} €
-                </p>
-              )}
+                <br />
+                {book.date && (
+                  <p className="mt-1 text-xs leading-5 text-gray-500">
+                    <span className="font-bold">Date : </span>
+                    {book.date}
+                  </p>
+                )}
+                {book.startedTime && book.endedTime && (
+                  <p className="mt-1 text-xs leading-5 text-gray-500">
+                    <span className="font-bold">Créneau : </span>
+                    {book.startedTime}h - {book.endedTime}h
+                  </p>
+                )}
+                {book.field && (
+                  <p className="mt-1 text-xs leading-5 text-gray-500">
+                    <span className="font-bold">Terrain : </span>
+                    {book.field}
+                  </p>
+                )}
+                {book.price && (
+                  <p className="mt-1 text-xs leading-5 text-gray-500">
+                    <span className="font-bold">Prix : </span>
+                    {book.price} €
+                  </p>
+                )}
+              </div>
             </div>
-          </div>
-          <Button text={"Annuler"} />
-        </li>
-      ))}
-    </ul>
+            <Button
+              text={"Annuler"}
+              onClick={() => handleDropBooking(book.id)}
+            />
+          </li>
+        ))}
+      </ul>
+    </>
   )
 }
