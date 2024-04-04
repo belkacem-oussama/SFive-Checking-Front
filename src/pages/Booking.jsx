f76a0cac472fdd813cbfb6f23dcd4ef3821e2eaa
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-04-04 14:09:05 +0200

    fix temp

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -44,6 +44,8 @@ export default function Booking({ listBooking, setListBooking }) {
 
   function formatTimeFromString(dateString) {
     const date = new Date(dateString)
+    // Ajoute 2 heures au temps
+    date.setHours(date.getHours() - 2)
     const hours = date.getHours().toString().padStart(2, "0")
     const minutes = date.getMinutes().toString().padStart(2, "0")
     return `${hours}:${minutes}`
