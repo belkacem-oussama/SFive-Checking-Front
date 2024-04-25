c6873bb840fca53501ec110c19f9a7b00311d36c
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-04-25 17:56:11 +0200

    +

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -167,7 +167,7 @@ export default function Booking({ listBooking, setListBooking }) {
 
       <ul role="list" className="divide-y divide-gray-100">
         {filteredBookings.length === 0 && (
-          <p className="flex items-center justify-center h-full text-gray-500">
+          <p className="flex items-center justify-center h-screen text-gray-500">
             Aucun résultats...
           </p>
         )}
