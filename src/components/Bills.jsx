016459fa05b60aba5d57872756416e838850672a
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-04-26 16:11:37 +0200

    style a little

--- src/components/Bills.jsx
+++ src/components/Bills.jsx
@@ -1,13 +1,35 @@
 import React from "react"
 import jsPDF from "jspdf"
+import SFiveLogo from "../assets/images/sfive_icone.png"
 
 export default function Bills() {
   const generatePDF = () => {
     const doc = new jsPDF()
 
-    doc.text("Exemple de facture", 10, 10)
+    // Logo SFive (hypothétique)
+    const logoWidth = 50
+    const logoHeight = 50
+    const logoX = 10
+    const logoY = 10
+    // Dessiner le logo
+    doc.addImage(SFiveLogo, "PNG", logoX, logoY, logoWidth, logoHeight)
 
-    doc.save("facture.pdf")
+    // Informations de la facture
+    const reservationNumber = "Réservation #12345"
+    const amount = "1000"
+    const companyName = "SFive"
+    const address = "6 Rue des Frères Peraux, 60180 Nogent-sur-Oise"
+
+    // Contenu de la facture
+    doc.setFontSize(12)
+    doc.text(`Numéro de réservation: ${reservationNumber}`, 10, 80)
+    doc.text(`Montant: ${amount} €`, 10, 90)
+    doc.text(`Facturé à:`, 10, 100)
+    doc.text(`${companyName}`, 15, 110)
+    doc.text(`${address}`, 15, 120)
+
+    // Sauvegarder le PDF avec un nom spécifique
+    doc.save("facture_SFIVE.pdf")
   }
 
   return (
