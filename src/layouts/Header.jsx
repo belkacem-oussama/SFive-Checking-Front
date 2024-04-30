fb4986aec4ab340c215891c50be9c44f310e8c35
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-04-30 12:17:11 +0200

    add pic

--- src/layouts/Header.jsx
+++ src/layouts/Header.jsx
@@ -5,6 +5,7 @@ import { Link, useLocation } from "react-router-dom"
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 
 import SFiveIMG from "../assets/images/sfive_icone.png"
+import ProfilePic from "../assets/images/profile.png"
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ")
@@ -118,7 +119,7 @@ export default function Header({ handeLogOut }) {
                           <span className="sr-only">Open user menu</span>
                           <img
                             className="h-8 w-8 rounded-full"
-                            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                            src={ProfilePic}
                             alt=""
                           />
                         </Menu.Button>
