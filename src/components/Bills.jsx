14d3e7c21734a5fe578a06ba118f145570c9b477
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-05-12 22:57:40 +0200

    Transfer bills to back_end

--- src/components/Bills.jsx
+++ src/components/Bills.jsx
@@ -1,9 +1,6 @@
 import React, { useState } from "react"
-import jsPDF from "jspdf"
-import "jspdf-autotable"
 import moment from "moment"
 import "moment/dist/locale/fr"
-import SFiveLogo from "../assets/images/sfive_icone.png"
 import Cookies from "js-cookie"
 import Alert from "../components/Alert.jsx"
 
@@ -17,139 +14,6 @@ export default function Bills({ listBooking, bookingId }) {
   const customerSurname = booking.customer.customer_surname
   const customerId = booking.customer.id
   const checkingId = booking.id
-
-  const generatePDF = () => {
-    const doc = new jsPDF()
-
-    // Logo SFive
-    const logoWidth = 40
-    const logoHeight = 30
-    const logoX = 10
-    const logoY = 10
-    doc.addImage(SFiveLogo, "PNG", logoX, logoY, logoWidth, logoHeight)
-
-    // Informations de l'entreprise
-    const companyName = "S-FIVE5"
-    const companyAddress = "6 Rue des Frères Peraux"
-    const companyCity = "60180 Nogent-sur-Oise"
-    const companyPhone = "03 65 09 03 90"
-    const companyEmail = "infosfives@gmail.com"
-
-    const customerName = customerFirstname + " " + customerSurname
-    const billsLocation = "À Nogent-Sur-Oise"
-
-    // Entreprise ------------------------------------
-    const companyInfoY = logoY + logoHeight + 5
-
-    // Définir la taille de police pour les informations de l'entreprise
-    const infoFontSize = 12
-
-    doc.setFontSize(infoFontSize)
-    doc.text(`${companyName}`, 20, companyInfoY)
-    doc.text(`${companyAddress}`, logoX, companyInfoY + 5)
-    doc.text(`${companyCity}`, logoX, companyInfoY + 10)
-    doc.text(`${companyPhone}`, logoX, companyInfoY + 15)
-    doc.text(`${companyEmail}`, logoX, companyInfoY + 20)
-
-    // Informations sur la facture
-    const invoiceDate = moment().locale("fr").format("LL")
-
-    // Client ------------------------------------
-    const clientInfoX = 150
-    const clientInfoY = companyInfoY + 30
-    doc.text(`${customerName}`, clientInfoX, clientInfoY)
-
-    // Date et lieu de facturation ----------------------
-    const billingInfoY = clientInfoY + 10
-    doc.text(`Le ${invoiceDate}`, clientInfoX, billingInfoY)
-    doc.text(`${billsLocation}`, clientInfoX, billingInfoY + 5)
-
-    // Objet de la facture -----------------------------
-    const invoiceObjectY = billingInfoY + 20
-    doc.text("Objet : Facture SFIVE5", 10, invoiceObjectY)
-
-    // Position numéro facture -------------------------
-    doc.text(`Facture ${invoiceNumber}`, 80, invoiceObjectY + 20)
-
-    // Tableau des articles -----------------------------
-    const items = [
-      {
-        description: `${moment(booking.checking_end).diff(
-          booking.checking_start,
-          "hours",
-          true
-        )} heure${
-          moment(booking.checking_end).diff(
-            booking.checking_start,
-            "hours",
-            true
-          ) !== 1
-            ? "s"
-            : ""
-        }`,
-
-        totalPriceHT: `${booking.checking_price * 0.8} €`,
-        totalPriceTTC: `${booking.checking_price} €`,
-        totalPay: `${booking.checking_price} €`,
-      },
-    ]
-
-    // Définition des colonnes et lignes du tableau
-    const tableColumns = [
-      { header: "Quantité", dataKey: "description" },
-      { header: "Prix Total HT", dataKey: "totalPriceHT" },
-      { header: "Prix Total TTC", dataKey: "totalPriceTTC" },
-      { header: "À Régler", dataKey: "totalPay" },
-    ]
-
-    const tableY = invoiceObjectY + 30 // Position Y pour le tableau des articles
-
-    doc.autoTable({
-      startY: tableY,
-      headStyles: {
-        fillColor: [51, 122, 183],
-        textColor: [255, 255, 255],
-        fontStyle: "bold",
-      },
-      bodyStyles: { fontSize: 14 },
-      columnStyles: { 0: { cellWidth: 60 } },
-      body: items,
-      columns: tableColumns,
-      didParseCell: function (data) {
-        if (
-          data.column.dataKey === "totalPay" &&
-          data.cell.section === "body"
-        ) {
-          data.cell.styles.fontStyle = "bold"
-          data.cell.text = `${data.cell.text}`
-        }
-      },
-    })
-
-    // Message  ------------------------------------
-    const totalAmountY = doc.autoTable.previous.finalY + 10
-    doc.text(
-      "Nous restons à votre disposition pour toute information complémentaire.",
-      13,
-      totalAmountY + 5
-    )
-    doc.text("Cordialement,", 13, totalAmountY + 10)
-    doc.text("L'équipe S-FIVE5", 13, totalAmountY + 15)
-
-    // SIRET / N° TVA -----------------------------------
-    const companyInfoBottomY = doc.internal.pageSize.height - 30
-
-    doc.setFont("helvetica", "bold")
-    doc.text(
-      "N° SIRET : 84937789000017 / N° TVA : FR 39849377890",
-      45,
-      companyInfoBottomY
-    )
-
-    // Sauvegarder le PDF avec un nom spécifique
-    doc.save(`facture_${invoiceNumber}`)
-    handleSendBills()
-  }
 
   const handleSendBills = async () => {
     try {
@@ -166,6 +30,8 @@ export default function Bills({ listBooking, bookingId }) {
             bills_ref: `${invoiceNumber}.pdf`,
             customer_id: customerId,
             checking_id: checkingId,
+            customer_firstname: customerFirstname,
+            customer_surname: customerSurname,
           }),
         }
       )
@@ -209,7 +75,7 @@ export default function Bills({ listBooking, bookingId }) {
       {showAlert && <Alert alertMessage="Facture générée." />}
       <button
         className=" px-4 py-2 flex items-center space-x-2 rounded-md bg-gray-800 text-white hover:bg-gray-900 "
-        onClick={generatePDF}
+        onClick={handleSendBills}
       >
         <p className="font-bold">Générer une facture</p>
         <svg
