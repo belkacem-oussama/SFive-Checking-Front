4d675638b971218ce2e0e885bc883fe7cb9887f9
BELKACEM Oussama <belkacem-oussama@macbook-pro-de-belkacem.home>
2024-05-16 00:40:14 +0200

    showAlert

--- src/pages/Price.jsx
+++ src/pages/Price.jsx
@@ -1,10 +1,13 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 import Cookies from "js-cookie"
 
 import Input from "../components/Input.jsx"
+import Alert from "../components/Alert.jsx"
 
 export default function Price({ prices, setPrices }) {
+  const [showAlert, setShowAlert] = useState(false)
+
   const handleChange = (e) => {
     const { name, value } = e.target
     setPrices((prevPrices) => ({
@@ -32,7 +35,10 @@ export default function Price({ prices, setPrices }) {
       )
 
       if (response.ok) {
-        console.log(response)
+        setShowAlert(true)
+        setTimeout(() => {
+          setShowAlert(false)
+        }, 3000)
       } else {
         console.error("Erreur lors de la requête:", response.status)
       }
@@ -42,93 +48,96 @@ export default function Price({ prices, setPrices }) {
   }
 
   return (
-    <div className="bg-white py-8 sm:py-16">
-      <div className="mx-auto max-w-7xl px-4 lg:px-6">
-        <div className="text-left">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-            Tarifs
-          </h2>
-          <p className="mt-4 text-lg leading-8 text-gray-600 border-b border-gray-900/10 pb-3">
-            Modifier les tarifs de réservation.
-          </p>
-          <form onSubmit={handleSubmit} className="mt-8 space-y-6">
-            <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
-              <div className="flex items-center space-x-4 p-2">
-                <label
-                  htmlFor="field_price_1"
-                  className="block text-sm font-medium text-gray-700 w-1/3"
-                >
-                  1 Heure
-                </label>
-                <Input
-                  name="field_price_1"
-                  inputValue={prices.field_price_1}
-                  onChange={handleChange}
-                  className="w-2/3"
-                />
+    <>
+      {showAlert && <Alert alertMessage="Tarifs modifiés." bgColor={false} />}
+      <div className="bg-white py-8 sm:py-16">
+        <div className="mx-auto max-w-7xl px-4 lg:px-6">
+          <div className="text-left">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Tarifs
+            </h2>
+            <p className="mt-4 text-lg leading-8 text-gray-600 border-b border-gray-900/10 pb-3">
+              Modifier les tarifs de réservation.
+            </p>
+            <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+              <div className="grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:gap-x-8">
+                <div className="flex items-center space-x-4 p-2">
+                  <label
+                    htmlFor="field_price_1"
+                    className="block text-sm font-medium text-gray-700 w-1/3"
+                  >
+                    1 Heure
+                  </label>
+                  <Input
+                    name="field_price_1"
+                    inputValue={prices.field_price_1}
+                    onChange={handleChange}
+                    className="w-2/3"
+                  />
+                </div>
+                <div className="flex items-center space-x-4 p-2">
+                  <label
+                    htmlFor="field_price_2"
+                    className="block text-sm font-medium text-gray-700 w-1/3"
+                  >
+                    1 Heure 30
+                  </label>
+                  <Input
+                    name="field_price_2"
+                    inputValue={prices.field_price_2}
+                    onChange={handleChange}
+                    className="w-2/3"
+                  />
+                </div>
+                <div className="flex items-center space-x-4 p-2">
+                  <label
+                    htmlFor="field_price_3"
+                    className="block text-sm font-medium text-gray-700 w-1/3"
+                  >
+                    2 Heures
+                  </label>
+                  <Input
+                    name="field_price_3"
+                    inputValue={prices.field_price_3}
+                    onChange={handleChange}
+                    className="w-2/3"
+                  />
+                </div>
+                <div className="flex items-center space-x-4 p-2">
+                  <label
+                    htmlFor="field_price_4"
+                    className="block text-sm font-medium text-gray-700 w-1/3"
+                  >
+                    Anniversaire
+                  </label>
+                  <Input
+                    name="field_price_4"
+                    inputValue={prices.field_price_4}
+                    onChange={handleChange}
+                    className="w-2/3"
+                  />
+                </div>
               </div>
-              <div className="flex items-center space-x-4 p-2">
-                <label
-                  htmlFor="field_price_2"
-                  className="block text-sm font-medium text-gray-700 w-1/3"
-                >
-                  1 Heure 30
-                </label>
-                <Input
-                  name="field_price_2"
-                  inputValue={prices.field_price_2}
-                  onChange={handleChange}
-                  className="w-2/3"
-                />
-              </div>
-              <div className="flex items-center space-x-4 p-2">
-                <label
-                  htmlFor="field_price_3"
-                  className="block text-sm font-medium text-gray-700 w-1/3"
-                >
-                  2 Heures
-                </label>
-                <Input
-                  name="field_price_3"
-                  inputValue={prices.field_price_3}
-                  onChange={handleChange}
-                  className="w-2/3"
-                />
-              </div>
-              <div className="flex items-center space-x-4 p-2">
-                <label
-                  htmlFor="field_price_4"
-                  className="block text-sm font-medium text-gray-700 w-1/3"
-                >
-                  Anniversaire
-                </label>
-                <Input
-                  name="field_price_4"
-                  inputValue={prices.field_price_4}
-                  onChange={handleChange}
-                  className="w-2/3"
-                />
-              </div>
-            </div>
-            <div className="flex justify-end p-2 space-x-4">
-              <Link to="/">
+              <div className="flex justify-end p-2 space-x-4">
+                <Link to="/">
+                  <button
+                    type="button"
+                    className="inline-flex justify-center py-2 px-4 border-none text-sm font-medium rounded-md text-gray-700  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                  >
+                    Annuler
+                  </button>
+                </Link>
                 <button
-                  type="button"
-                  className="inline-flex justify-center py-2 px-4 border-none text-sm font-medium rounded-md text-gray-700  hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500"
+                  type="submit"
+                  className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 >
-                  Annuler
+                  Valider
                 </button>
-              </Link>
-              <button
-                type="submit"
-                className="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              >
-                Valider
-              </button>
-            </div>
-          </form>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   )
 }
